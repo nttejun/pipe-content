2891d6f92f8322e61ed907a7b0b2924bd48342e9
nttejun <nttejun@gmail.com>
2022-06-10 15:19:11 +0900

    [Add] 프로젝트 생성

---
+++ src/main/java/com/bu/pipecontent/PipeContentApplication.java
@@ -0,0 +1,13 @@
+package com.bu.pipecontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipeContentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bu/pipecontent/PipeContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bu.pipecontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
